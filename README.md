c414b4163a1bd128b3140b79a98981c1dfe71190
yangyi724 <yeungstar@163.com>
2020-06-30 17:35:53 +0800

    log in : get user information from github

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 ## 问答系统
+
+
+
+## GitHub 登录
+
+[Creating an OAuth App](https://developer.github.com/apps/building-oauth-apps/creating-an-oauth-app/)
+
+[OkHttp 发出 POST](https://square.github.io/okhttp/)
---
+++ src/main/java/com/june/project/community/Provider/GithubProvider.java
@@ -0,0 +1,63 @@
+package com.june.project.community.Provider;
+
+import com.alibaba.fastjson.JSON;
+import com.june.project.community.dto.AccessTokenDTO;
+import com.june.project.community.dto.GithubUser;
+import okhttp3.*;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+/**
+ * @author June
+ * @date 2020/6/30 - 15:18
+ */
+@Component
+public class GithubProvider {
+    /*
+     * 3. 携带 accessTokenDTO 中的 code 信息(首先要把信息从Java类型转化成JSON类型)
+     *    向 Github 的 access_token API (https://github.com/login/oauth/access_token) 发出 POST，获取 AccessToken
+     *    access_token=e72e16c7e42f292c6912e7710c838347ae178b4a&token_type=bearer
+     * 4. 获取到 access_token，返回 access_token
+     */
+    public String getAccessToken(AccessTokenDTO accessTokenDTO) {
+        MediaType mediaType = MediaType.get("application/json; charset=utf-8");
+        OkHttpClient client = new OkHttpClient();
+
+        RequestBody body = RequestBody.create(mediaType, JSON.toJSONString(accessTokenDTO));
+        Request request = new Request.Builder()
+                .url("https://github.com/login/oauth/access_token")
+                .post(body)
+                .build();
+        try (Response response = client.newCall(request).execute()) {
+            String string = response.body().string();
+            String token = string.split("&")[0].split("=")[1];
+            return token;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    /*
+     * 5. 携带 access_token Github 的 user API (https://api.github.com/user?access_token=) 发出 Request，获取用户信息
+     * 6. 获取到JSON格式的用户信息，解析成Java类格式的用户信息 GithubUser
+     */
+    public GithubUser getUser(String accessToken) {
+
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder()
+                .url("https://api.github.com/user?access_token=" + accessToken)
+                .build();
+
+        try {
+            Response response = client.newCall(request).execute();
+            String string = response.body().string();
+            GithubUser githubUser = JSON.parseObject(string, GithubUser.class);
+            return githubUser;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
