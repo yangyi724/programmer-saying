e42fe607c4e79744c75fbea2c73ab2de2b535054
yangyi724 <yeungstar@163.com>
2020-07-02 12:12:44 +0800

    my question

---
+++ src/main/java/com/june/project/community/controller/ProfileController.java
@@ -0,0 +1,70 @@
+package com.june.project.community.controller;
+
+import com.june.project.community.dto.PaginationDTO;
+import com.june.project.community.mapper.UserMapper;
+import com.june.project.community.model.User;
+import com.june.project.community.service.QuestionService;
+import jdk.nashorn.internal.ir.FunctionNode;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * @author June
+ * @date 2020/7/2 - 10:21
+ */
+@Controller
+public class ProfileController {
+
+    @Autowired
+    private QuestionService questionService;
+
+    @Autowired
+    private UserMapper userMapper;
+
+    // 希望调用 profile.html 的时候访问 ProfileController
+    @GetMapping("/profile/{action}")
+    public String profile(HttpServletRequest request,
+                          @PathVariable(name = "action") String action,
+                          Model model,
+                          @RequestParam(name = "page", defaultValue = "1") Integer page,      // 分页 1.：page 分页的页码， size 分页数
+                          @RequestParam(name = "size", defaultValue = "5") Integer size) {
+        User user = null;
+        Cookie[] cookies = request.getCookies();
+        if(cookies != null && cookies.length != 0) {
+            for(Cookie cookie : cookies) {
+                if(cookie.getName().equals("token")) {
+                    String token = cookie.getValue();
+                    user = userMapper.findByToken(token);
+                    if(user != null) {
+                        request.getSession().setAttribute("user", user);
+                    }
+                    break;
+                }
+            }
+        }
+
+        if(user == null) {
+            return "redirect:/";
+        }
+
+        if("questions".equals(action)) {
+            model.addAttribute("section", "questions");
+            model.addAttribute("sectionName", "我的提问");
+        } else if("replies".equals(action)){
+            model.addAttribute("section", "replies");
+            model.addAttribute("sectionName", "最新回复");
+        }
+
+        PaginationDTO paginationDTO = questionService.list(user.getId(), page, size);
+        model.addAttribute("pagination", paginationDTO);  // 引号中的是前端的变量名
+        // 表示返回到 profile.html
+        return "profile";
+    }
+}
--- src/main/java/com/june/project/community/service/QuestionService.java
+++ src/main/java/com/june/project/community/service/QuestionService.java
@@ -32,19 +32,68 @@ public class QuestionService {
 
         PaginationDTO paginationDTO = new PaginationDTO();
         Integer totalCount = questionMapper.count(); // 分页 3. ：通过 mapper 数据库查询到 totalCount 记录的总数
-        paginationDTO.setPagination(totalCount, page, size); // 分页 4. ：通过 totalCount, page, size 算出分页需要的其他数据，如是否展示上一页，下一页，首尾页等
+
+
+        Integer totalPage;
+        if (totalCount % size == 0) {
+            totalPage = totalCount / size;
+        } else {
+            totalPage = totalCount / size + 1;
+        }
 
         // 校验页码
         if (page < 1) {
             page = 1;
         }
-        if (page > paginationDTO.getTotalPage()) {
-            page = paginationDTO.getTotalPage();
+        if (page > totalPage) {
+            page = totalPage;
         }
+
+        paginationDTO.setPagination(totalPage, page); // 分页 4. ：通过 totalCount, page, size 算出分页需要的其他数据，如是否展示上一页，下一页，首尾页等
 
         // offset : size*(page-1)
         Integer offset = size * (page - 1);
         List<Question> questions = questionMapper.list(offset, size);
+        List<QuestionDTO> questionDTOList = new ArrayList<>();
+        for (Question question : questions) {
+            User user = userMapper.findById(question.getCreator());
+            QuestionDTO questionDTO = new QuestionDTO();
+            BeanUtils.copyProperties(question, questionDTO);
+            questionDTO.setUser(user);
+            questionDTOList.add(questionDTO);
+        }
+        paginationDTO.setQuestions(questionDTOList);
+        return paginationDTO;
+    }
+
+    public PaginationDTO list(int userId, Integer page, Integer size) {
+        PaginationDTO paginationDTO = new PaginationDTO();
+        Integer totalCount = questionMapper.countByUserId(userId); // 分页 3. ：通过 mapper 数据库查询到 totalCount 记录的总数
+
+
+        Integer totalPage;
+        if (totalCount % size == 0) {
+            totalPage = totalCount / size;
+        } else {
+            totalPage = totalCount / size + 1;
+        }
+
+        // 校验页码
+        if (page < 1) {
+            page = 1;
+        }
+        if (page > totalPage) {
+            page = totalPage;
+        }
+
+
+        paginationDTO.setPagination(totalPage, page); // 分页 4. ：通过 totalCount, page, size 算出分页需要的其他数据，如是否展示上一页，下一页，首尾页等
+
+
+
+        // offset : size*(page-1)
+        Integer offset = size * (page - 1);
+        List<Question> questions = questionMapper.listByUserId(userId, offset, size);
         List<QuestionDTO> questionDTOList = new ArrayList<>();
         for (Question question : questions) {
             User user = userMapper.findById(question.getCreator());
